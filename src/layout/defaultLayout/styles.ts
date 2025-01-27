35d779eccea6328609fc6aa014a0c2f9bd9e22d7
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-27 20:44:47 -0300

    definindo layout base da aplicaçao

---
+++ src/layout/defaultLayout/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+   height: 100vh;
+   max-width: 74rem;
+
+   margin: auto;
+
+   display: flex;
+   flex-direction: column;
+`
--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Success } from "./pages/success";
 import { Checkout } from "./pages/checkout";
+import { DefaultLayout } from "./layout/defaultLayout";
 
 export function Router() {
    return (
       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/success" element={<Success />} />
-         <Route path="/checkout" element={<Checkout />} />
+         <Route path="/" element={<DefaultLayout />} >
+            <Route path="/" element={<Home />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/checkout" element={<Checkout />} />
+         </Route>
       </Routes>
    )
 }
