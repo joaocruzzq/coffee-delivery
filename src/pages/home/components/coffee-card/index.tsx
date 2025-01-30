57b9ef627a3437649eb084ebf0c65568c79ac40a
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-30 16:30:59 -0300

    separando lista de cafes

--- src/pages/home/components/coffee-card/index.tsx
+++ src/pages/home/components/coffee-card/index.tsx
@@ -4,33 +4,42 @@ import { ShoppingCart } from "phosphor-react";
 
 import { Stepper } from "../../../../components/stepper";
 
-interface CoffeeProps {
+interface TagProps {
+   id: number
+   name: string
+}
+
+interface CoffeeType {
    id: number
    name: string
    price: number
    image: string
-   tags: string[]
+   tags: TagProps[]
    description: string
 }
 
-export function CoffeeCard(props: CoffeeProps) {
+interface CoffeeProps {
+   coffee: CoffeeType
+}
+
+export function CoffeeCard({coffee}: CoffeeProps) {
    return (
       <CoffeeCardContainer>
-         <img src={props.image} alt="" />
+         <img src={coffee.image} alt="" />
 
          <div className="tags">
-            {props.tags.map(tag => {
+            {coffee.tags.map(tag => {
                return (
-                  <strong>{tag}</strong>
+                  <strong key={tag.id}>{tag.name}</strong>
                )
             })}
          </div>
 
-         <h1>{props.name}</h1>
-         <span>{props.description}</span>
+         <h1>{coffee.name}</h1>
+         <span>{coffee.description}</span>
 
          <div className="buy-section">
-            <h1>{props.price.toFixed(2)}</h1>
+            <h1>{coffee.price.toFixed(2)}</h1>
 
             <div className="actions">
                <Stepper />
---
+++ src/utils/coffeeList.ts
@@ -0,0 +1,335 @@
+export const coffeeTypesList = [
+   {
+      id: 1,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Expresso Tradicional',
+      image: 'src/assets/Type=Expresso.png',
+      description: 'O tradicional café feito com água quente e grãos moídos',
+   },
+
+   {
+      id: 2,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Expresso Americano',
+      image: 'src/assets/Type=Americano.png',
+      description: 'Expresso diluído, menos intenso que o tradicional',
+   },
+
+   {
+      id: 3,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Expresso Cremoso',
+      image: 'src/assets/Type=Expresso Cremoso.png',
+      description: 'Café expresso tradicional com espuma cremosa',
+   },
+
+   {
+      id: 4,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'gelado'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Expresso Gelado',
+      image: 'src/assets/Type=Café Gelado.png',
+      description: 'Bebida preparada com café expresso e cubos de gelo',
+   },
+
+   {
+      id: 5,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Café com Leite',
+      image: 'src/assets/Type=Café com Leite.png',
+      description: 'Meio a meio de expresso tradicional com leite vaporizado',
+   },
+
+   {
+      id: 6,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Latte',
+      image: 'src/assets/Type=Latte.png',
+      description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+   },
+
+   {
+      id: 7,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Capuccino',
+      image: 'src/assets/Type=Capuccino.png',
+      description: 'Bebida com canela feita de doses iguais de café, leite e espuma',
+   },
+
+   {
+      id: 8,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Macchiato',
+      image: 'src/assets/Type=Macchiato.png',
+      description: 'Café expresso misturado com um pouco de leite quente e espuma',
+   },
+
+   {
+      id: 9,
+
+      tags: [
+         {
+            id: 1,
+            name: 'tradicional'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Mochaccino',
+      image: 'src/assets/Type=Mochaccino.png',
+      description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+   },
+
+   {
+      id: 10,
+
+      tags: [
+         {
+            id: 1,
+            name: 'especial'
+         },
+
+         {
+            id: 2,
+            name: 'com leite'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Chocolate Quente',
+      image: 'src/assets/Type=Chocolate Quente.png',
+      description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+   },
+
+   {
+      id: 11,
+
+      tags: [
+         {
+            id: 1,
+            name: 'especial'
+         },
+
+         {
+            id: 2,
+            name: 'alcoólico'
+         },
+
+         {
+            id: 3,
+            name: 'gelado'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Cubano',
+      image: 'src/assets/Type=Cubano.png',
+      description: 'Drink gelado de café expresso com rum, creme de leite e hortelã',
+   },
+
+   {
+      id: 12,
+
+      tags: [
+         {
+            id: 1,
+            name: 'especial'
+         },
+
+         {
+            id: 2,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Havaiano',
+      image: 'src/assets/Type=Havaiano.png',
+      description: 'Bebida adocicada preparada com café e leite de coco',
+   },
+
+   {
+      id: 13,
+
+      tags: [
+         {
+            id: 1,
+            name: 'especial'
+         },
+
+         {
+            id: 2,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Árabe',
+      image: 'src/assets/Type=Árabe.png',
+      description: 'Bebida preparada com grãos de café árabe e especiarias',
+   },
+
+   {
+      id: 14,
+
+      tags: [
+         {
+            id: 1,
+            name: 'especial'
+         },
+
+         {
+            id: 2,
+            name: 'alcoólico'
+         },
+
+         {
+            id: 3,
+            name: 'quente'
+         }
+      ],
+
+      price: 9.90,
+      name: 'Irlandês',
+      image: 'src/assets/Type=Irlandês.png',
+      description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
+   },
+]
